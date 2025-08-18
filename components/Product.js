e40685d92bdf04f00cd7dde5d14a8c714b5b0276
YasharFRD <yasharfaridy@gmail.com>
2025-08-18 10:55:01 +0330

    Create page for each products

--- components/Product.js
+++ components/Product.js
@@ -1,32 +1,30 @@
 import Link from "next/link";
 
-function Product({product}) {
-   return (
-    <div className="card bg-base-300 shadow-lg flex flex-col">
+function Product({ product }) {
+  return (
+    <div className="flex flex-col shadow-lg card bg-base-300">
       <Link href={`/product/${product.slug_link}`}>
         <a>
           <figure className="aspect-[4/3] overflow-hidden rounded-t-2xl">
             <img
               src={product.image_src}
               alt={product.title}
-              className="w-full h-full object-cover"
+              className="object-cover w-full h-full"
             />
           </figure>
         </a>
       </Link>
 
-      <div className="card-body flex flex-col flex-grow">
+      <div className="flex flex-col flex-grow card-body">
         <Link href={`/product/${product.slug_link}`}>
           <a>
-            <h2 className="card-title text-lg line-clamp-2">
-              {product.title}
-            </h2>
+            <h2 className="text-lg card-title line-clamp-2">{product.title}</h2>
           </a>
         </Link>
 
-        <h3 className="font-bold mt-2">{product.price} $</h3>
+        <h3 className="mt-2 font-bold">{product.price} $</h3>
 
-        <div className="card-actions mt-auto justify-center">
+        <div className="justify-center mt-auto card-actions">
           <button className="btn btn-primary">Add to cart</button>
         </div>
       </div>
