e66b3168526c03a392ab10ab8b79756225d41e15
YasharFRD <yasharfaridy@gmail.com>
2025-08-18 09:28:55 +0330

    Show products in home page

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
 import Layout from "../components/Layout";
+import Product from "../components/Product";
+import productItems from '../data/products.json'
 
 export default function Home() {
   return (
     <Layout title="Home page">
-      {" "}
-      Home page
-      <div>
-        <button className="btn btn-primary">Dracula Theme</button>
-      </div>
+     <div className="grid gap-8 px-4 sm:px-6 md:px-8 lg:px-12 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      {productItems.map((product)=>(
+        <Product product={product} key={product.id}></Product>
+      ))}
+     </div>
     </Layout>
   );
 }
