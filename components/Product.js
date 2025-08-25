28a6c02c590d16e75346e4da288d8d68cee58390
YasharFRD <yasharfaridy@gmail.com>
2025-08-26 01:34:45 +0330

    create add to cart handler function for product page and main page

--- components/Product.js
+++ components/Product.js
@@ -1,6 +1,26 @@
 import Link from "next/link";
+import { useContext } from "react";
+import { CartContext } from "../contexts/Cart";
 
 function Product({ product }) {
+  const { state, dispatch } = useContext(CartContext);
+
+  function addToCartHandler() {
+    const existingProduct = state.cart.cartItems.find(
+      (cartItem) => cartItem.id === product.id
+    );
+
+    const qty = existingProduct ? existingProduct.qty + 1 : 1;
+
+    if (product.count < qty) {
+      alert("Product is out!");
+      return;
+    }
+
+    dispatch({ type: "ADD_CART_ITEM", payload: { ...product, qty } });
+    console.log("added");
+  }
+
   return (
     <div className="flex flex-col shadow-lg card bg-base-300">
       <Link href={`/product/${product.slug_link}`}>
@@ -24,7 +44,9 @@ function Product({ product }) {
         <h3 className="mt-2 font-bold">${product.price}</h3>
 
         <div className="justify-center mt-auto card-actions">
-          <button className="btn btn-primary">Add to cart</button>
+          <button className="btn btn-primary" onClick={addToCartHandler}>
+            Add to cart
+          </button>
         </div>
       </div>
     </div>
