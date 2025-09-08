b2b453c93a3666ced7af2cac17ce9d4548b179ad
YasharFRD <yasharfaridy@gmail.com>
2025-09-08 13:04:12 +0330

    create user model

---
+++ models/userModel.js
@@ -0,0 +1,12 @@
+import { defaults } from "autoprefixer";
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, require: true },
+  email: { type: String, require: true },
+  password: { type: String, require: true },
+  isAdmin: { type: Boolean, require: true, default: false },
+});
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+export default User;
