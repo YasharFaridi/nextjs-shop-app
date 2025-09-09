8ec0616142798f3e64973f3b375e5dfc10630ec2
YasharFRD <yasharfaridy@gmail.com>
2025-09-09 10:41:16 +0330

    fix connection bog

--- utils/db.js
+++ utils/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 function connect() {
-  mongoose.connect("mongodb:// 127.0.0.1:27017/redrumShop");
+  mongoose.connect("mongodb://127.0.0.1:27017/redrumShop");
 
   console.log("connected");
 }
